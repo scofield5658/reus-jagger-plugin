7aedeb631afe3330746f641e0b3f28d1044c2aac
gfguozhi <gfguozhi@gf.com.cn>
2020-01-07 09:57:41 +0800

    [fix] env

--- src/configs/webpack.config.js
+++ src/configs/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
       // new ExtractTextPlugin("styles.css"),
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: `"${process.env.REUS_PROJECT_ENV}"`,
+          NODE_ENV: `"${process.env.NODE_ENV}"`,
           REUS_PROJECT_ENV: `"${process.env.REUS_PROJECT_ENV}"`,
           BASE_URL: `"${config.baseUrl}"`,
           CDN_URL: `"${config.cdnUrl}"`,
